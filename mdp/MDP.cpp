341bab70ccca3e3832e2c5fa1ed135d60efceafb
Kaiu8x <kawaswim@gmail.com>
2019-11-19 12:35:47 -0600

    ya esta

--- mdp/MDP.cpp
+++ mdp/MDP.cpp
@@ -39,53 +39,43 @@ bool MDP::didConverge(){
 void MDP::doMDP(){
 	std::cout << "DO MDP!"<<endl;
 	int move;
-	int l;
 	int n = 0;
+	int l;
 	float probability = 0.0f;
 	float calculateValue = 0.0f;
-	float currentMax = std::numeric_limits<float>::min();
+	float currentMax = -std::numeric_limits<float>::max();
+	int inX[9] = {0,1,1,0,-1,-1,-1,0,1};
+	int inY[9] = {0,0,-1,-1,-1,0,1,1,1};
 	do {
 		std::cout << "ENTERED DO: "<< n <<endl;
 		for(int i = 1; i<REAL_BOARD_SIZE; i++) {
 			for (int j = 1; j <REAL_BOARD_SIZE; j++) {
-				l = 0; //just a counter to change the probabilities
 				move = 0; //move that has to be updated to new grid
 				for(int k = 1; k<9; k++) {
-					// change probability value
-					if(l == k) {
-						probability = 0.9f;
-					} else {
-						probability = 0.014f;
-					}
 
-					//check right
-					calculateValue += probability * valueGrid[i+1][j];
-					//check down right
-					calculateValue += probability * valueGrid[i+1][j-1];
-					//check down
-					calculateValue += probability * valueGrid[i][j-1];
-					//check left down
-					calculateValue += probability * valueGrid[i-1][j-1];
-					//check left
-					calculateValue += probability * valueGrid[i-1][j];
-					//check up left
-					calculateValue += probability * valueGrid[i-1][j+1];
-					//check up
-					calculateValue += probability * valueGrid[i][j+1];
-					//check up right
-					calculateValue += probability * valueGrid[i+1][j+1];
+					for(l = 1; l<9; l++) {
+						// change probability value
+						if(l == k) {
+							probability = 0.9f;
+						} else {
+							probability = 0.014f;
+						}
+						calculateValue += probability * valueGrid[i+inX[l]][j+inY[l]];
+					}
 
 					if(calculateValue > currentMax) {
+						//std::cout << "change value ENTERED" << endl;
 						currentMax = calculateValue;
-						move = l;
+						move = k;
 					}
-					l++;
+					calculateValue = 0.0f;
+					
 				}
 				nextValueGrid[i][j] = originalvalueGrid[i][j]+currentMax*gamma;
 				nextRules[i][j] = move;
-
+				//std::cout << "for i: "<< i <<" j: "<< j<< " new value: "<<nextValueGrid[i][j] << " new move: "<< nextRules[i][j] << endl; 
 				calculateValue = 0.0f;
-				currentMax = std::numeric_limits<float>::min();
+				currentMax = -std::numeric_limits<float>::max();
 			}
 		}
 
@@ -126,35 +116,67 @@ int main() {
 	float rewardGrid[BOARD_SIZE][BOARD_SIZE];
 	int equilibrium = 80;
 	for(int i = 1; i< REAL_BOARD_SIZE; i++) {
-		for(int j = 1; j<REAL_BOARD_SIZE; j++) {
-			if(i = j) {
-				rewardGrid[i][j] = -0.2f;
-			} else if(j > equilibrium && i > equilibrium) {
-				rewardGrid[i][j] = -0.1f;
-			} else if (j > i) {
-				rewardGrid[i][j] = -0.4f;
+		for(int j = 1; j< REAL_BOARD_SIZE; j++) {
+			if(i == j) {
+				rewardGrid[i][j] = -2.0f;
+			} else if(j >= equilibrium && i >= equilibrium) {
+				rewardGrid[i][j] = -1.0f;
+			} else if (j >= i) {
+				rewardGrid[i][j] = -4.0f;
 			} else if (i > j) {
-				rewardGrid[i][j] = -0.3f;
+				rewardGrid[i][j] = -3.0f;
 			}
 		}
 	}
-	rewardGrid[equilibrium][equilibrium] = 10;
-	rewardGrid[1][1] = -20;
-	rewardGrid[100][1] = -5;
-	rewardGrid[1][100] = -8;
-	rewardGrid[100][100] = -10;
+	rewardGrid[equilibrium][equilibrium] = 100;
+	rewardGrid[equilibrium+1][equilibrium] = 100;
+	rewardGrid[equilibrium][equilibrium+1] = 100;
+	rewardGrid[equilibrium+1][equilibrium+1] = 100;
+	rewardGrid[equilibrium-1][equilibrium] = 100;
+	rewardGrid[equilibrium][equilibrium-1] = 100;
+	rewardGrid[equilibrium-1][equilibrium-1] = 100;
+
+
+	rewardGrid[1][1] = -200;
+	rewardGrid[1][2] = -200;
+	rewardGrid[2][1] = -200;
+	rewardGrid[2][2] = -200;
+	
+	rewardGrid[100][1] = -50;
+	rewardGrid[100][2] = -50;
+	rewardGrid[99][2] = -50;
+	rewardGrid[99][1] = -50;
+
+	rewardGrid[1][100] = -80;
+	rewardGrid[1][99] = -80;
+	rewardGrid[2][100] = -80;
+	rewardGrid[2][99] = -80;
+
+	rewardGrid[100][100] = -100;
+	rewardGrid[99][100] = -100;
+	rewardGrid[100][99] = -100;
+	rewardGrid[99][99] = -100;
+
+	//for (int i = 1; i < BOARD_SIZE-1; i++) { 
+	//   for (int j = 1; j < BOARD_SIZE-1; j++) { 
+	//      std::cout << rewardGrid[i][j] << " "; 
+	//   } 
+	//     
+	//   std::cout << endl; 
+	//}
+	//std::cout << endl;
+	//std::cout << endl;
+	//std::cout << endl;
+	//std::cout << endl;
+	//std::cout << endl;
 
 	MDP * mdp = new MDP(rewardGrid,0.8);
-	std::cout << "HW!"<<endl;
 	mdp->doMDP();
-	std::cout << "HW 2 !!"<<endl;
-	//std::cout << "Hello World!"<<endl;
 
 	for (int i = 1; i < BOARD_SIZE-1; i++) { 
 	   for (int j = 1; j < BOARD_SIZE-1; j++) { 
 	      std::cout << mdp->nextRules[i][j] << " "; 
 	   } 
-	     
 	   std::cout << endl; 
 	}
 }
